68bcadd29dba85f7e63ca78c48f345bed386565b
MoviesFreakZone <deviouns@gmail.com>
2025-11-15 19:19:18 +0100

    Update auto_publish.js

--- auto_publish.js
+++ auto_publish.js
@@ -1,6 +1,4 @@
 // Recarga automática cada 2 minutos (120000ms)
 setInterval(() => {
-  const event = new Event("input");
-  document.getElementById("search-input").dispatchEvent(event); // Fuerza re-render con búsqueda actual
   fetchItems(); // recarga datos
 }, 120000);
