65501d9a3d82702684f6910f8cfd92d3315ffef2
MoviesFreakZone <deviouns@gmail.com>
2025-11-29 19:08:36 +0100

    Delete auto_publish.js

--- auto_publish.js
+++
@@ -1,6 +0,0 @@
-// auto_publish.js
-// recarga cada 60 segundos
-setInterval(()=> {
-    // llama a fetchData definida en catalogo.js
-    if (typeof fetchData === 'function') fetchData();
-}, 60000);
